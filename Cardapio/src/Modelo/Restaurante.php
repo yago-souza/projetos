81c174f74f0b6a05999e2a38580ced6f8e6c2efd
Yago <yagos-s@live.com>
2023-01-18 21:11:33 -0300

    Criando uma classe no php

---
+++ Cardapio/src/Modelo/Restaurante.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cardapio\Modelo\Restaurante;
+
+class Restaurante
+{
+    private Nome $nome;
+    private Endereco $endereco;
+    private Nota $nota;
+
+    public function __construct(Nome $nome, Endereco $endereco, Nota )
+    {
+    }
+
+}
