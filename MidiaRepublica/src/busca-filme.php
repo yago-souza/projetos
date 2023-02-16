4919b2f1944b9bb3085afc847f1c799fd0bdb28c
Yago <yagos-s@live.com>
2023-02-15 22:39:58 -0300

    Projeto de buscar CEP

---
+++ MidiaRepublica/src/busca-filme.php
@@ -0,0 +1,6 @@
+<?php
+
+$chaveAPI = 'a1f81ae4b7b848e00e517101ae8c1d79';
+$idFilme = 120;
+## faz a pesquisa pelo id do filme
+$url = "https://api.themoviedb.org/3/search/movie/$idFilme/api_key=$chaveAPI&language=pt-BR";
