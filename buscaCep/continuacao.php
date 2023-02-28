e3dd0c65b2fbe1e2fb3df73a06536754a7386f23
Yago <yagos-s@live.com>
2023-02-28 16:34:52 -0300

    Att

--- buscaCep/continuacao.php
+++ buscaCep/continuacao.php
@@ -22,13 +22,9 @@ $sql = "SELECT CEP
                WHERE ID = '$ultimoID'";
 $retornoSql = consultaSQL($conexao, $sql);
 $ultimoCEP = $retornoSql[0];
-#var_dump($retornoSql);
 
     for ($i = $ultimoCEP; $i<=99999999; $i++) {
-
         $id = $ultimoID++;
-        #var_dump($i);
-
         $cep = str_pad($i, '8', '0', STR_PAD_LEFT);
         ##Consulta CEP na view SPL_S_CEP pelo ID
         $sql = "SELECT A.*,
@@ -37,7 +33,6 @@ $ultimoCEP = $retornoSql[0];
                        WHERE CEP = '$cep'
                        ";
         $retornoSql = consultaSQL($conexao, $sql);
-        #$cepSql = $retornoSql['CEP'];
 
             if($retornoSql == false) {
                 try {
@@ -47,23 +42,21 @@ $ultimoCEP = $retornoSql[0];
                     if ($result == new stdClass()) {
                         throw new \Exception("Objeto vazio");
                     } else {
-                        echo "<p>Sem erro</p>";
                         $endereco = new Endereco(
                             $result->return->cep,
-                            $result->return->end,
-                            $result->return->complemento2,
-                            $result->return->bairro,
-                            $result->return->cidade,
-                            $result->return->uf
+                                $result->return->end,
+                                    $result->return->complemento2,
+                                        $result->return->bairro,
+                                            $result->return->cidade,
+                                                $result->return->uf
                         );
                         $cepS = $endereco->getCep();
-                        $endS = $endereco->getEnd();
-                        $complemento2S = $endereco->getComplemento2();
-                        $bairroS = $endereco->getBairro();
-                        $cidadeS = $endereco->getCidade();
-                        $ufS = $endereco->getUf();
+                            $endS = $endereco->getEnd();
+                                $complemento2S = $endereco->getComplemento2();
+                                    $bairroS = $endereco->getBairro();
+                                        $cidadeS = $endereco->getCidade();
+                                            $ufS = $endereco->getUf();
                         ## Inserer as informaçoes na tabela S_CEP
-
                         $sql = "INSERT INTO S_CEP VALUES (
                                                               '$cepS',
                                                               '$cidadeS',
@@ -75,87 +68,11 @@ $ultimoCEP = $retornoSql[0];
                                                               '$id'
                                                             )";
                         $insertSql = consultaSQL($conexao, $sql);
-
-                        echo "Retorno do Correio: ". $cepS . ', ' . $endS . ', '. $bairroS . ', ' . $cidadeS . ', ' . $ufS . ', ' . $complemento2S;
+                        #echo "Retorno do Correio: ". $cepS . ', ' . $endS . ', '. $bairroS . ', ' . $cidadeS . ', ' . $ufS . ', ' . $complemento2S;
                     }
                 } catch (\Exception $exception) {
                     echo $exception->getMessage() . PHP_EOL;
                 }
         }
     }
-
-
-        /*
-        ##Consulta CEP na tabela S_CEP pelo CEP
-        $sql = "SELECT A.*
-                           FROM S_CEP A
-                           WHERE CEP = '$cepSql'";
-        $retornoS_CEP = consultaSQL($conexao, $sql);
-
-        ## Se o cep existir pula para o próximo cep
-        if ($retornoS_CEP != false) {
-            /*#echo "<h2>O CEP ". $retornoSql['CEP'] . " já existe na tabela S_CEP.</h2>";
-            #echo "<p>Endereço antigo (SPL_S_CEP): " .
-                $retornoSql['ID'] . " " .
-                $retornoSql['CEP_STREET'] . ", " .
-                $retornoSql['CEP_BAIRRO'] . ", " .
-                $retornoSql['CEP_CITY'] . ", " .
-                $retornoSql['CEP_STATE'] . ", " .
-                $retornoSql['CEP'] . ", " .
-                $retornoSql['CEP_ROTA'] . "</p>";
-
-            #echo "<p>Endereço novo (S_CEP): " . " " .
-                $retornoS_CEP['CEP_STREET'] . ", " .
-                $retornoS_CEP['CEP_BAIRRO'] . ", " .
-                $retornoS_CEP['CEP_CITY'] . ", " .
-                $retornoS_CEP['CEP_STATE'] . ", " .
-                $retornoS_CEP['CEP'] . " " .
-                $retornoS_CEP['CEP_ROTA'] . " " .
-                $retornoS_CEP['OBSERVACAO'] . "</p>";
-        } else {
-            #echo "<p>O CEP ". $retornoSql['CEP'] . " não existe na tabela S_CEP.</p>";
-            ## consulta o número do CEP no Web Service
-            try {
-                $arguments = array('consultaCEP' => array('cep' => $cepSql));
-                $result = $client->__soapCall($function, $arguments);
-                if ($result == new stdClass()) {
-                    throw new \Exception("Objeto vazio");
-                } else {
-                    $endereco = new Endereco(
-                        $result->return->cep,
-                        $result->return->end,
-                        $result->return->complemento2,
-                        $result->return->bairro,
-                        $result->return->cidade,
-                        $result->return->uf
-                    );
-                    $cepS = $endereco->getCep();
-                    $endS = $endereco->getEnd();
-                    $complemento2S = $endereco->getComplemento2();
-                    $bairroS = $endereco->getBairro();
-                    $cidadeS = $endereco->getCidade();
-                    $ufS = $endereco->getUf();
-                    ## Inserer as informaçoes na tabela S_CEP
-                    $sql = "INSERT INTO S_CEP VALUES (
-                                                      '$cepS',
-                                                      '$cidadeS',
-                                                      '$ufS',
-                                                      '$endS',
-                                                      '$bairroS',
-                                                      null,
-                                                      '$complemento2S',
-                                                      '$i'
-                                                    )";
-                    $insertSql = consultaSQL($conexao, $sql);
-                    echo "Retorno do Correio: ". $cepS . ', ' . $endS . ', '. $bairroS . ', ' . $cidadeS . ', ' . $ufS . ', ' . $complemento2S;
-                }
-            } catch (\Exception $exception) {
-                echo $exception->getMessage() . PHP_EOL;
-            }
-        }
-    }
-*/
-#}
-
-
 
